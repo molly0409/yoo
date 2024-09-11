505b26faf8aa0a285b2ecff41bb7fb3a25d848e9
molly0409 <molly0409@naver.com>
2024-09-11 14:24:34 +0900

    summation

--- week02/2411941 week2.c
+++ week02/2411941 week2.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(int argc,char *argv[]){
+	int a=10;//정수 변수 a와 b를 지정 
+	int b=20;
+	
 	printf("Hello, Sookmyung Electronics!\n");
+	printf("%d + %d = %d\n",a,b,a+b);
 	
 	return 0;
 }
