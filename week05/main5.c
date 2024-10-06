e0a2ca3c9ed5d523dd442a0b6cf225a5fded5290
molly0409 <molly0409@naver.com>
2024-10-06 16:50:50 +0900

    .

--- week05/main5.c
+++ week05/main5.c
@@ -3,19 +3,15 @@
 int main(int argc, char *argv[]) {
 	
 	int a,b;
-	int x,y;
 	
 	printf("Input two integers: ");
 	scanf("%i %i",&a,&b);
 	
-	x=a<<1;
-	y=a>>1;
-	
 	printf("& result is %d\n",a&b);
 	printf("| result is %d\n",a|b);
 	printf("^ result is %d\n",a^b);
-	printf("<<1 result is %d\n",x);
-	printf(">>1 result is %d\n",y);
+	printf("<<1 result is %d\n",a<<1);
+	printf(">>1 result is %d\n",a>>1);
 	
 	return 0;
 }
