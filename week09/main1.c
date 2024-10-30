adb1101bc4667217afdff2aee661b9f9016e8870
molly0409 <molly0409@naver.com>
2024-10-30 14:58:24 +0900

    09

---
+++ week09/main1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*int main(void) 
+{
+	int flag=1;
+	while(flag!=0)
+	{
+		int y;
+		y=3;
+		flag=0;
+	}
+	y=4;
+}*/
+
+int main(void) 
+{
+
+	int flag=1;
+	int y;
+	while(flag!=0)
+	{
+		y=3;
+		flag=0;
+	}
+	y=4;
+}
